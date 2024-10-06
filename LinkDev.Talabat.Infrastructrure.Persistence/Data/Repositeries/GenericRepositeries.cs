f0c90691465f4f30b9d6e27a5a59efff0f66c4e7
Ahmed Adel <Ahmed_Adel_2415@fci.helwan.edu.eg>
2024-10-06 20:56:01 +0300

    Generic Repository

---
+++ LinkDev.Talabat.Infrastructrure.Persistence/Data/Repositeries/GenericRepositeries.cs
@@ -0,0 +1,31 @@
+﻿using LinkDev.Talabat.Core.Domain.Common;
+using LinkDev.Talabat.Core.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Infrastructrure.Persistence.Data.Repositeries
+{
+    public class GenericRepositeries<TEntity, TKey>(StoreDbContxt dbContxt) : IGenericRepositeries<TEntity, TKey>
+        where TEntity : BaseEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool withTracking = false)
+         =>  withTracking ? await dbContxt.Set<TEntity>().ToListAsync() : await dbContxt.Set<TEntity>().AsNoTracking().ToListAsync();
+
+        public async Task<TEntity?> GetAsync(TKey id)
+               =>  await dbContxt.Set<TEntity>().FindAsync();
+
+        public async Task AddAsync(TEntity entity)
+            => await dbContxt.Set<TEntity>().AddAsync(entity); 
+
+        public void Update(TEntity entity)
+         => dbContxt.Update<TEntity>(entity);
+
+        public void Delete(TEntity entity)
+          => dbContxt.Set<TEntity>().Remove(entity);
+
+    }
+}
