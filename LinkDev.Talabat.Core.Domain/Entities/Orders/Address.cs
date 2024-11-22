ec6bcc7ebb9497ed8102d7b9bbcaf8526fb16116
Ahmed Adel <Ahmed_Adel_2415@fci.helwan.edu.eg>
2024-11-22 19:50:41 +0200

    Order Module - Entities

---
+++ LinkDev.Talabat.Core.Domain/Entities/Orders/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Orders
+{
+    // owned to Order Entity
+    //[Owned]
+    public class Address
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string street { get; set; }
+        public required string  City { get; set; }
+        public required string  Country { get; set; }
+    }
+}
---
+++ LinkDev.Talabat.Core.Domain/Entities/Orders/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Orders
+{
+    public class Order
+    {
+        public required string BuyerEmail { get; set; }
+        public DateTime OrderTime { get; set; }
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public required Address ShippingAddress { get; set; }
+        
+        
+        public int? deliveryMethodId { get; set; }
+        public DeliveryMethod? deliveryMethod { get; set; }
+
+        public required ICollection<OrderItem> OrderItems { get; set; } = new HashSet<OrderItem>();
+
+        public decimal SupTotal { get; set; }
+        //[NotMapped]
+        //public decimal Total { get{ return SupTotal + deliveryMethod!.Cost; } }
+
+        // Getter Method
+        public decimal GetTotal => SupTotal + deliveryMethod!.Cost;
+
+        public string PaymentIntenedId { get; set; } = "";
+    }
+}
