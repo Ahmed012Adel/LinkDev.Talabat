ed75aa4326a72e175e894a6eef6cfefd95821a11
Ahmed Adel <Ahmed_Adel_2415@fci.helwan.edu.eg>
2024-11-23 18:17:25 +0200

    Order Service and Dtos

---
+++ LinkDev.Talabat.Core.Application.Abstraction/Order/Models/CreateOrderDto.cs
@@ -0,0 +1,16 @@
+﻿using LinkDev.Talabat.Core.Application.Abstraction.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Application.Abstraction.Order.Models
+{
+    public class CreateOrderDto
+    {
+        public required string BasketId { get; set; }
+        public int DeliveryMethod { get; set; }
+        public AddressDto ShippingAddress { get; set; }
+    }
+}
---
+++ LinkDev.Talabat.Core.Application/Mapping/OrderPictureUrlResolver.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using LinkDev.Talabat.Core.Application.Abstraction.Order.Models;
+using LinkDev.Talabat.Core.Domain.Entities.Orders;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LinkDev.Talabat.Core.Application.Mapping
+{
+    internal class OrderPictureUrlResolver(IConfiguration configuration) : IValueResolver<OrderItem, OrderItemsDto, string>
+    {
+      
+        public string Resolve(OrderItem source, OrderItemsDto destination, string destMember, ResolutionContext context)
+        {
+            if(!source.Product.PictureUrl.IsNullOrEmpty())
+                return $"{configuration["URLs:BaseUrl"]}/{source.Product.PictureUrl}";
+
+            return string.Empty;
+        }
+    }
+}
