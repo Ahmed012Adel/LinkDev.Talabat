0c0a87bda0521ca62da78e1aaca3ab23a2da69f8
Ahmed Adel <Ahmed_Adel_2415@fci.helwan.edu.eg>
2024-11-24 01:40:55 +0200

    Order Module - Configurartion Classes

---
+++ LinkDev.Talabat.Infrastructrure.Persistence/_Data/Config/OrderConfig/DeliveryMethodConfugirations.cs
@@ -0,0 +1,20 @@
+﻿using LinkDev.Talabat.Core.Domain.Entities.Orders;
+using LinkDev.Talabat.Infrastructrure.Persistence.Data.Config.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Infrastructrure.Persistence.Data.Config.OrderConfig
+{
+    internal class DeliveryMethodConfugirations : BaseEntityConfiguration<DeliveryMethod , int>
+    {
+        public override void Configure(EntityTypeBuilder<DeliveryMethod> builder)
+        {
+            base.Configure(builder);
+            builder.Property(Method => Method.Cost)
+                .HasColumnType("decimal(8,2)");
+        }
+    }
+}
