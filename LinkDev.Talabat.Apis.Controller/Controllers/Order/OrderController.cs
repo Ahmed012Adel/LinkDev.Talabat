5df89ba1db3a9d99eaba6094f8b4bf32b58263e9
Ahmed Adel <Ahmed_Adel_2415@fci.helwan.edu.eg>
2024-11-24 13:55:34 +0200

    GetOrdersForUser, GetOrderForUser, GetDeliveryMethods endpoints

--- LinkDev.Talabat.Apis.Controller/Controllers/Order/OrderController.cs
+++ LinkDev.Talabat.Apis.Controller/Controllers/Order/OrderController.cs
@@ -2,19 +2,14 @@
 using LinkDev.Talabat.Core.Application.Abstraction;
 using LinkDev.Talabat.Core.Application.Abstraction.Order.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LinkDev.Talabat.Apis.Controller.Controllers.Order
 {
     public class OrderController(IServiceManager serviceManager) : ApiControllerBase
     {
-        [HttpGet] 
-        public async Task<ActionResult<OrderToReturnDto>> CreateOrderAsync(CreateOrderDto order)
+        [HttpPost]
+        public async Task<ActionResult<OrderToReturnDto>> CreateOrder(CreateOrderDto order)
         {
             var buyerEmail = User.FindFirstValue(ClaimTypes.Email);
             var Order = await serviceManager.OrdersService.CreateOrderAsync(buyerEmail!, order);
@@ -22,6 +17,31 @@ namespace LinkDev.Talabat.Apis.Controller.Controllers.Order
             return Ok(Order);
         }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderToReturnDto>>> GetOrdersForUser()
+        {
+            var buyerEmail = User.FindFirstValue(ClaimTypes.Email);
+            var result = await serviceManager.OrdersService.GetOrderForUserAsync(buyerEmail!);
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")] // base/Api/Order/id
+        public async Task<ActionResult<IEnumerable<OrderToReturnDto>>> GetOrder(int id)
+        {
+            var buyerEmail = User.FindFirstValue(ClaimTypes.Email);
+            var result = await serviceManager.OrdersService.GetOrderByIdAsync(buyerEmail!, id);
+            
+            return Ok(result);
+        }
+
+        [HttpGet("deliveryMethods")]// base/Api/Order/deliveryMethods
+
+        public async Task<ActionResult<IEnumerable<DeliveryMethodeDto>>> GetDeliveryMethod()
+        {
+            var result = await serviceManager.OrdersService.GetDeliveryMethodAsync();
+            return Ok(result);
+        }
 
     }
 }
